726c832cdca9a6287baf4dc4a40ad8bd253cd9df
cladams0203 <cladams0203@gmail.com>
2022-05-05 10:57:40 -0400

    implemented redux solution

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import { legacy_createStore } from "redux";
+import reducer from "./state/reducers";
+import "bulma/css/bulma.css";
+import "./styles.scss";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+const store = legacy_createStore(reducer);
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
